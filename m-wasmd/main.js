8b5e1e7f66c3cdb254e7503f989de5727f36696e
coffee <39930080+coffee522jelly-development@users.noreply.github.com>
2021-07-14 12:43:27 +0900

    Add files via upload
    first-commit
    まだ動きません

---
+++ m-wasmd/main.js
@@ -0,0 +1,19 @@
+/*
+ * Runstant
+ * 思いたったらすぐ開発. プログラミングに革命を...
+ */
+
+
+window.onload = async () => {
+  // 準備を待つ
+  await markdown.ready;
+  
+  $input.oninput = () => {
+    // 入力されるたびに変換する
+    var md = markdown.parse($input.value);
+    $output.innerHTML = md;
+  };
+  
+  // 一回実行しておく
+  $input.oninput();
+};
