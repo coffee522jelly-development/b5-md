845db4f8e9815b26c42a312fecacb7760c79da63
coffee <39930080+coffee522jelly-development@users.noreply.github.com>
2021-07-17 23:17:43 +0900

    リストリアルタイム
    ・リストのリアルタイム変換に対応しました。

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ window.onload = async () => {
     // 入力されるたびに変換する
     var md = markdown.parse($input.value);
     $output.innerHTML = md;
+
+    // 要素にクラスを追加
+    $("ul").attr({class : "list-group"});
+    $("ol").attr({class : "list-group list-group-numbered"});
+    $("li").attr({class : "list-group-item"});
   };
-  
+
   // 一回実行しておく
   $input.oninput();
 };
