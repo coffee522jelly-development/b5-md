4ec32f0dc23f0143932f1831261ed9532b425ffd
coffee <39930080+coffee522jelly-development@users.noreply.github.com>
2021-08-03 12:40:11 +0900

    Update index.js
    クオート追加

--- index.js
+++ index.js
@@ -105,6 +105,28 @@ $(function(){
   });
 });
 
+ // クオートボタンを押した
+ $(function(){
+  $('#Quote').click(function(){
+   const getSelectArea = () => {
+     const val = $('textarea').val(),
+     elm = $('textarea').get(0),
+     tmp = {
+     before: val.slice(0, elm.selectionStart),
+     after: val.slice(elm.selectionEnd),
+     text: elm.value.slice(elm.selectionStart, elm.selectionEnd),
+     };
+     return tmp;
+     };
+     let sel = getSelectArea()
+     if (!sel.text) return;
+     let txt = '> ' + sel.text;
+     $('textarea').val(sel.before + txt + sel.after);
+
+     $input.oninput();
+  });
+});
+
  // 線ボタンを押した
  $(function(){
   $('#Line').click(function(){
